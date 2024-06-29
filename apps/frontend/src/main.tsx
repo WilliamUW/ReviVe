55c73f2bf002857c5b2d87084f8153e13174eab0
William Wang <bwilliamwang@gmail.com>
2024-06-29 15:08:03 -0700

    working route

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { BrowserRouter as Router } from "react-router-dom";
+import './styles/index.css'; // Import your Tailwind CSS file
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
--- apps/frontend/src/pages/Broken.tsx
+++ apps/frontend/src/pages/Broken.tsx
@@ -1,5 +1,30 @@
-import React from 'react';
+import {Flex, Container} from "@chakra-ui/react";
+import React from "react";
+import {InfoCard, Instructions, Footer, Dropzone} from "../components";
 
-const Broken = () => <div>Broken Page</div>;
+const Broken = () => (
+  <div>
+    Broken Page
+    <>
+      <Flex flex={1}>
+        <Container
+          mt={{ base: 4, md: 10 }}
+          maxW={"container.xl"}
+          mb={{ base: 4, md: 10 }}
+          display={"flex"}
+          flex={1}
+          alignItems={"center"}
+          justifyContent={"flex-start"}
+          flexDirection={"column"}
+        >
+          <InfoCard />
+          <Instructions />
+          <Dropzone />
+        </Container>
+      </Flex>
+      <Footer />
+    </>
+  </div>
+);
 
 export default Broken;
