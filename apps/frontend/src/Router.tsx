20dc8282cd4247881fc6a6d4c0308116d1f5de2d
William Wang <bwilliamwang@gmail.com>
2024-06-29 14:52:37 -0700

    global routing

--- apps/frontend/src/Router.tsx
+++ apps/frontend/src/Router.tsx
@@ -1,15 +1,15 @@
 // src/Router.jsx
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './pages/Home';
-import Unused from './pages/Unused';
-import Broken from './pages/Broken';
-import Buy from './pages/Buy';
-import App from './App'; // Your main app component
-import BottomNavbar from './components/BottomNavbar';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Unused from "./pages/Unused";
+import Broken from "./pages/Broken";
+import Buy from "./pages/Buy";
+import App from "./App"; // Your main app component
+import BottomNavbar from "./components/BottomNavbar";
 
 const AppRouter = () => (
-  <Router>
+  <>
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/unused" element={<Unused />} />
@@ -17,7 +17,7 @@ const AppRouter = () => (
       <Route path="/buy" element={<Buy />} />
     </Routes>
     <BottomNavbar />
-  </Router>
+  </>
 );
 
 export default AppRouter;
