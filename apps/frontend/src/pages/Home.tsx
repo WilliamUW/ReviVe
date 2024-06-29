e79680dec8c7a2594ad02ec132280b460bbca557
William Wang <bwilliamwang@gmail.com>
2024-06-29 15:40:24 -0700

    Update Home.tsx

--- apps/frontend/src/pages/Home.tsx
+++ apps/frontend/src/pages/Home.tsx
@@ -1,40 +1,47 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Dropzone, InfoCard, Instructions } from "../components";
-import { Flex, Container, Button, VStack } from "@chakra-ui/react";
-import { useUserContext } from "../components/UserContext";
+import { Box, VStack, Heading, Button } from "@chakra-ui/react";
 
-const Home = () => {
-  const { listings } = useUserContext();
-  
+export default function Home() {
   return (
-    <VStack spacing={4} align="center">
-      <Link to="/sell" style={{ textDecoration: 'none' }}>
-        <Button
-          width="256px"
-          height="64px"
-          backgroundColor="blue.500"
-          color="white"
-          _hover={{ backgroundColor: "blue.600" }}
-          transition="background-color 0.2s"
-        >
-          I have electronics
-        </Button>
-      </Link>
-      <Link to="/buy" style={{ textDecoration: 'none' }}>
-        <Button
-          width="256px"
-          height="64px"
-          backgroundColor="green.500"
-          color="white"
-          _hover={{ backgroundColor: "green.600" }}
-          transition="background-color 0.2s"
-        >
-          I want electronics
-        </Button>
-      </Link>
-    </VStack>
+    <Box 
+      as="main" 
+      minHeight="100vh" 
+      display="flex" 
+      flexDirection="column" 
+      alignItems="center" 
+      justifyContent="center" 
+      padding={24}
+    >
+      <Heading as="h1" size="2xl" marginBottom={8}>
+        Welcome to ReviVe
+      </Heading>
+      <VStack spacing={4} align="stretch" width="256px">
+        <Link to="/sell" style={{ textDecoration: 'none' }}>
+          <Button
+            width="100%"
+            height="64px"
+            backgroundColor="blue.500"
+            color="white"
+            _hover={{ backgroundColor: "blue.600" }}
+            transition="background-color 0.2s"
+          >
+            I have electronics
+          </Button>
+        </Link>
+        <Link to="/buy" style={{ textDecoration: 'none' }}>
+          <Button
+            width="100%"
+            height="64px"
+            backgroundColor="green.500"
+            color="white"
+            _hover={{ backgroundColor: "green.600" }}
+            transition="background-color 0.2s"
+          >
+            I want electronics
+          </Button>
+        </Link>
+      </VStack>
+    </Box>
   );
-};
-
-export default Home;
+}
