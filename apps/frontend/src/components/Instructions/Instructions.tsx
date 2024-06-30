18984fb438d1aa9feee50a2f60dea3be3893371d
William Wang <bwilliamwang@gmail.com>
2024-06-30 11:31:44 -0700

    Update Instructions.tsx

--- apps/frontend/src/components/Instructions/Instructions.tsx
+++ apps/frontend/src/components/Instructions/Instructions.tsx
@@ -4,17 +4,17 @@ import { Step } from "./Step";
 const Steps = [
   {
     icon: "/steps/1.svg",
-    title: "Fix electronics",
+    title: "38 Electronics Fixed",
     description: "Repair or refurbish electronics to extend their lifespan.",
   },
   {
     icon: "/steps/2.svg",
-    title: "List electronics",
+    title: "58 Electronics Listed",
     description: "List your repaired or refurbished electronics for sale.",
   },
   {
     icon: "/steps/3.svg",
-    title: "Buy electronics",
+    title: "25 Electronics Bought",
     description: "Purchase repaired or refurbished electronics from others.",
   },
 ];
