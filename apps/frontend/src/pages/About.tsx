4d906f37769b7c53009be5719153221e1badc467
William Wang <bwilliamwang@gmail.com>
2024-06-30 01:08:03 -0700

    Update About.tsx

--- apps/frontend/src/pages/About.tsx
+++ apps/frontend/src/pages/About.tsx
@@ -1,10 +1,51 @@
 import { Flex, Container } from "@chakra-ui/react";
-import { InfoCard, Instructions, Footer, Dropzone } from "../components";
-import BuyCoffee from "./BuyCoffee";
+import { Instructions } from "../components";
 
+const CanvaEmbed = () => (
+  <>
+    <div
+      style={{
+        position: "relative",
+        width: "100%",
+        height: 0,
+        paddingTop: "56.25%",
+        paddingBottom: 0,
+        boxShadow: "0 2px 8px 0 rgba(63,69,81,0.16)",
+        marginTop: "1.6em",
+        marginBottom: "0.9em",
+        overflow: "hidden",
+        borderRadius: "8px",
+        willChange: "transform",
+      }}
+    >
+      <iframe
+        loading="lazy"
+        style={{
+          position: "absolute",
+          width: "100%",
+          height: "100%",
+          top: 0,
+          left: 0,
+          border: "none",
+          padding: 0,
+          margin: 0,
+        }}
+        src="https://www.canva.com/design/DAGJjT0jOkc/zbI_qjXO0kiELWwtmTl_sw/view?embed"
+        allowFullScreen
+        allow="fullscreen"
+      ></iframe>
+    </div>
+    <a
+      href="https://www.canva.com/design/DAGJjT0jOkc/zbI_qjXO0kiELWwtmTl_sw/view?utm_content=DAGJjT0jOkc&utm_campaign=designshare&utm_medium=embeds&utm_source=link"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      ReviVe
+    </a>
+  </>
+);
 const About = () => (
   <>
-    <BuyCoffee />
     <Flex flex={1}>
       <Container
         mt={{ base: 4, md: 10 }}
@@ -16,12 +57,14 @@ const About = () => (
         justifyContent={"flex-start"}
         flexDirection={"column"}
       >
-        <InfoCard />
-        <Instructions />
-        <Dropzone />
+                <Instructions />
+
+        <CanvaEmbed />
+        {/* <InfoCard /> */}
+        {/* <Dropzone /> */}
       </Container>
     </Flex>
-    <Footer />
+    {/* <Footer /> */}
   </>
 );
 
